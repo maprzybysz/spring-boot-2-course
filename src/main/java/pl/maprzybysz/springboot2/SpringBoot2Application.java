6f9834334d9fd0e3db8e371507cf504ffe11e85c
Mateusz Przybysz <notentego678@gmaill.com>
2021-04-16 17:33:50 +0200

    Setting up a project.

---
+++ src/main/java/pl/maprzybysz/springboot2/SpringBoot2Application.java
@@ -0,0 +1,20 @@
+package pl.maprzybysz.springboot2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+
+@SpringBootApplication
+public class SpringBoot2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot2Application.class, args);
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void get(){
+        System.out.println("Hello world");
+    }
+
+}
