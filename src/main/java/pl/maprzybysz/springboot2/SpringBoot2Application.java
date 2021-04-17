d9adeccf5979480a991cd93df783ba3efea82889
Mateusz Przybysz <notentego678@gmaill.com>
2021-04-17 18:11:15 +0200

    Internationalization.

--- src/main/java/pl/maprzybysz/springboot2/SpringBoot2Application.java
+++ src/main/java/pl/maprzybysz/springboot2/SpringBoot2Application.java
@@ -3,13 +3,24 @@ package pl.maprzybysz.springboot2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.event.EventListener;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 
 @SpringBootApplication
 public class SpringBoot2Application {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBoot2Application.class, args);
+    }
+
+
+    @Bean
+    public ReloadableResourceBundleMessageSource messageSource(){
+        ReloadableResourceBundleMessageSource source = new ReloadableResourceBundleMessageSource();
+        source.setBasename("classpath:i18n/messages");
+        source.setDefaultEncoding("UTF-8");
+        return source;
     }
 
 
