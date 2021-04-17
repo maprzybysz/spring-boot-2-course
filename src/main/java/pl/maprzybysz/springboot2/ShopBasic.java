ad0e6c053204858951ed57a728e49a66def04099
Mateusz Przybysz <notentego678@gmaill.com>
2021-04-17 18:54:07 +0200

    Profiling.

---
+++ src/main/java/pl/maprzybysz/springboot2/ShopBasic.java
@@ -0,0 +1,16 @@
+package pl.maprzybysz.springboot2;
+
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@Profile("Basic")
+public class ShopBasic {
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void get() {
+        System.out.println("ShopBasic");
+    }
+}
