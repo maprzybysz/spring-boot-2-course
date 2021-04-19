da782d1e7cb084d091d60df6aed8f0bc82d14f4c
Mateusz Przybysz <notentego678@gmaill.com>
2021-04-19 10:26:05 +0200

    Special characters in configuration.

--- src/main/java/pl/maprzybysz/springboot2/ShopService.java
+++
@@ -1,8 +0,0 @@
-package pl.maprzybysz.springboot2;
-
-import java.util.List;
-
-public interface ShopService {
-     void printShoppingCart(List<Product> productList);
-     double calculateTotalPrice(List<Product> productList);
-}
