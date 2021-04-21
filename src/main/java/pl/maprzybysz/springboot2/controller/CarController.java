a37a48d4a4865d458a80036449b931b0e9a26630
Mateusz Przybysz <notentego678@gmaill.com>
2021-04-21 12:44:08 +0200

    Thymeleaf V.

--- src/main/java/pl/maprzybysz/springboot2/controller/CarController.java
+++ src/main/java/pl/maprzybysz/springboot2/controller/CarController.java
@@ -3,9 +3,7 @@ package pl.maprzybysz.springboot2.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import pl.maprzybysz.springboot2.model.Car;
 
 import java.util.ArrayList;
@@ -39,6 +37,25 @@ public class CarController {
     @PostMapping("/add-car")
     public String addCar(@ModelAttribute Car car){
         cars.add(car);
+        return "redirect:/car";
+    }
+    @GetMapping("/delete-car/{id}")
+    public String deleteCar(@PathVariable int id){
+        cars.remove(id);
+        return "redirect:/car";
+    }
+    @GetMapping("/edit-car/{id}")
+    public String showUpdateForm(@PathVariable int id, Model model){
+        Car car = cars.get(id);
+        model.addAttribute("editCar", car);
+        model.addAttribute("id", id);
+        return "car-update";
+    }
+    @PostMapping("/update-car")
+    public String updateCar(@RequestParam int id, @ModelAttribute Car car){
+        Car editCar = cars.get(id);
+        editCar.setMark(car.getMark());
+        editCar.setModel(car.getModel());
         return "redirect:/car";
     }
 }
