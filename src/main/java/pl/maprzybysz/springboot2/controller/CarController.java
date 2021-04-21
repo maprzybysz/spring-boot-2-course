80a892b64d53781679bb41876d83aebc0b0b3d0f
Mateusz Przybysz <notentego678@gmaill.com>
2021-04-21 11:31:59 +0200

    Thymeleaf IV.

--- src/main/java/pl/maprzybysz/springboot2/controller/CarController.java
+++ src/main/java/pl/maprzybysz/springboot2/controller/CarController.java
@@ -4,24 +4,41 @@ package pl.maprzybysz.springboot2.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import pl.maprzybysz.springboot2.model.Car;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Controller
 public class CarController {
 
-    @GetMapping("/car")
-    public String get(Model model){
+    private List<Car> cars;
+
+    public CarController() {
         Car car1 = new Car("BMW", "M2");
         Car car2 = new Car("BMW", "M3");
         Car car3 = new Car("BMW", "M4");
         Car car4 = new Car("BMW", "M5");
-        List<Car> cars = Arrays.asList(car1, car2, car3, car4);
+        cars = new ArrayList<>();
+        cars.add(car1);
+        cars.add(car2);
+        cars.add(car3);
+        cars.add(car4);
+    }
 
+    @GetMapping("/car")
+    public String get(Model model){
         model.addAttribute("cars", cars);
-
+        model.addAttribute("newCar", new Car());
         return "car";
+    }
+
+    @PostMapping("/add-car")
+    public String addCar(@ModelAttribute Car car){
+        cars.add(car);
+        return "redirect:/car";
     }
 }
