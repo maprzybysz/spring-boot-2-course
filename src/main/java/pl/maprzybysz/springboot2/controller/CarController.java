df393cd98c580e6d01a79db3c68bda9df30c3599
Mateusz Przybysz <notentego678@gmaill.com>
2021-04-21 11:04:20 +0200

    Thymeleaf III.

--- src/main/java/pl/maprzybysz/springboot2/controller/CarController.java
+++ src/main/java/pl/maprzybysz/springboot2/controller/CarController.java
@@ -6,14 +6,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import pl.maprzybysz.springboot2.model.Car;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Controller
 public class CarController {
 
     @GetMapping("/car")
     public String get(Model model){
-        Car car = new Car("BMW", "M3");
-        model.addAttribute("name", "Mateusz");
-        model.addAttribute("car", car);
-        return "hello";
+        Car car1 = new Car("BMW", "M2");
+        Car car2 = new Car("BMW", "M3");
+        Car car3 = new Car("BMW", "M4");
+        Car car4 = new Car("BMW", "M5");
+        List<Car> cars = Arrays.asList(car1, car2, car3, car4);
+
+        model.addAttribute("cars", cars);
+
+        return "car";
     }
 }
