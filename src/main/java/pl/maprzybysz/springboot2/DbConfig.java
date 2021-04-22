bdefd0c6a0beeee65c361dc0f3a3c52e2fb8dc62
Mateusz Przybysz <notentego678@gmaill.com>
2021-04-22 17:22:19 +0200

    Setting-up-a-connection.

---
+++ src/main/java/pl/maprzybysz/springboot2/DbConfig.java
@@ -0,0 +1,48 @@
+package pl.maprzybysz.springboot2;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.EventListener;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DbConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public DbConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    //    @Bean
+//    public DataSource getDataSource(){
+//        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+//        dataSourceBuilder.url("jdbc:mysql://localhost:3306/ksb2");
+//        dataSourceBuilder.username("root");
+//        dataSourceBuilder.password("zaq1@WSX");
+//        dataSourceBuilder.driverClassName("com.mysql.cj.jdbc.Driver");
+//        return dataSourceBuilder.build();
+//    }
+
+
+
+    @Bean
+    public JdbcTemplate getJdbcTemplate(){
+        return new JdbcTemplate(dataSource);
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void init(){
+//        String sql = "CREATE TABLE videos(video_id int, title varchar(255), url varchar(255), PRIMARY KEY (video_id))";
+        String sql1 = "INSERT videos VALUES (3, 'dsa', 123)";
+//        getJdbcTemplate().update(sql);
+        getJdbcTemplate().update(sql1);
+    }
+
+}
