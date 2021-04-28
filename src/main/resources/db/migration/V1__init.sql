5d9b8743f8a72e8fa77cb92f398929b163cb6e20
Mateusz Przybysz <notentego678@gmaill.com>
2021-04-28 17:35:34 +0200

    DTO.

--- src/main/resources/db/migration/V1__init.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS vehicle;
-
-CREATE TABLE vehicle (
-                         id INT AUTO_INCREMENT,
-                         brand VARCHAR(250) NOT NULL,
-                         model VARCHAR(250) NOT NULL,
-                         color VARCHAR(250) NOT NULL,
-                         PRIMARY KEY(id)
-);
--- src/main/resources/db/migration/V2__insert-data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO vehicle (brand, color, model) VALUES
-('Alfa Romeo', 'Gulietta', 'red');
